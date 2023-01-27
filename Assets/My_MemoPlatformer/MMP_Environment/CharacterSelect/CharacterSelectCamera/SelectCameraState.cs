ebe61c89284126a2901fd7a6a8aa322f0fa60bf2
DigitalCatharsis <shinegumy@gmail.com>
2023-01-27 23:14:12 +0300

    Soul
    unfinished cam

---
+++ Assets/My_MemoPlatformer/MMP_Environment/CharacterSelect/CharacterSelectCamera/SelectCameraState.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace My_MemoPlatformer
+{
+
+
+
+    public class SelectCameraState : StateMachineBehaviour
+    {
+
+        // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            PlayableCharacterType[] arr = System.Enum.GetValues(typeof(PlayableCharacterType)) as PlayableCharacterType[];
+        
+
+        foreach (PlayableCharacterType p in arr)
+            {
+                animator.SetBool(p.ToString(), false);
+            }
+        }
+    }
+}
