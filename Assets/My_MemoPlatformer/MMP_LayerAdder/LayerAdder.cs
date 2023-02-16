5b25b148443e3a5da405133f5a1bc76b2aac9250
Soul <shinegumy@gmail.com>
2023-02-16 17:49:37 +0300

    MMP_LayerAdder

---
+++ Assets/My_MemoPlatformer/MMP_LayerAdder/LayerAdder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace My_MemoPlatformer
+{
+    public enum MMP_Layers
+    {
+        PostProcessing,
+        CHARACTER,
+        BOT,
+        GROUND,
+    }
+
+    public class LayerAdder : MonoBehaviour
+    {
+
+    }
+}
