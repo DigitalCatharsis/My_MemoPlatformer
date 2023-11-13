5ea66a1a8e1cd8afa6bd78eed9e7cdae07cc7d23
Soul <shinegumy@gmail.com>
2023-11-13 12:40:41 +0300

    LedgeGrab Offset change

--- Assets/My_MemoPlatformer/MMP_Characters/States/Abilities_StateScripts/OffsetOnLedge.cs
+++ Assets/My_MemoPlatformer/MMP_Characters/States/Abilities_StateScripts/OffsetOnLedge.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Rendering.Universal;
 using UnityEngine;
 
 namespace My_MemoPlatformer
@@ -8,42 +9,15 @@ namespace My_MemoPlatformer
         public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
         {
             
-            GameObject anim = characterState.characterControl.skinnedMeshAnimator.gameObject;
-            anim.transform.parent = characterState.characterControl.ledgeChecker.grabbedLedge.transform;
-            anim.transform.localPosition = characterState.characterControl.ledgeChecker.grabbedLedge.offset;
-
-            #region ledgeGrabCalibration  //untested
-            float x;
-            float y;
-            float z;
-
-            if (characterState.characterControl.IsFacingForward())
-            {
-                x = characterState.characterControl.ledgeChecker.ledgeCalibration.x;
-                y = characterState.characterControl.ledgeChecker.ledgeCalibration.y;
-                z = characterState.characterControl.ledgeChecker.ledgeCalibration.z;
-            }
-            else
-            {
-                x = characterState.characterControl.ledgeChecker.ledgeCalibration.x;
-                y = characterState.characterControl.ledgeChecker.ledgeCalibration.y;
-                z = -characterState.characterControl.ledgeChecker.ledgeCalibration.z;
-            }
-
-            Vector3 calibration;
-            calibration.x = x;
-            calibration.z = z;
-            calibration.y = y;
-
-            anim.transform.localPosition += calibration;
-            #endregion
-
-            characterState.characterControl.Rigid_Body.velocity= Vector3.zero;
         }
 
         public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
         {
-
+            if (!characterState.characterControl.Rigid_Body.useGravity)
+            {
+                characterState.characterControl.Rigid_Body.MovePosition(characterState.characterControl.ledgeChecker.grabbedLedge.transform.position
+                    + characterState.characterControl.ledgeChecker.grabbedLedge.offset);           
+            }
         }
 
         public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
